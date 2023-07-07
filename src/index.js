8d4540f3b485d0c00a131f938f254f13776e934d
anthony_f <afanous@hotmail.com>
2023-07-07 12:36:48 -0400

    Name tag generator template built

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles.css';
 import App from './App';
-import Confetti from 'react-confetti';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <Confetti />
   </React.StrictMode>
 );
